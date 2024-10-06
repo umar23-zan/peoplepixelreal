6562743c60042aa6c1e5f1a47129856347defd9d
umar23-zan <umar.zangroups@gmail.com>
2024-10-06 23:04:54 +0530

    individual contacts file created

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import Header from './Header'
+import Sidebar from './Sidebar';
+import Father from './imageFolder/Father.png'
+import Mother from './imageFolder/Mother.png'
+import Brother from './imageFolder/Brother.png'
+import Sister from './imageFolder/Sister.png'
+import ElderSister from './imageFolder/ElderSister.png'
+import GrandFather from './imageFolder/GrandFather.png'
+import GrandMother from './imageFolder/GrandMother.png'
+
+
+
 
 const Contacts = () => {
-  return (
-    <div>Contacts</div>
-  )
+   const contentItems =[
+    { title: 'Father', img:Father, alt:'father'},
+    { title: 'Mother', img:Mother, alt:'mother'},
+    { title: 'Brother', img:Brother, alt:'brother'},
+    { title: 'Elder Sister', img:ElderSister, alt:'eldersister'},
+    { title: 'Sister', img:Sister, alt:'sister'},
+    { title: 'Grand Father', img:GrandFather, alt:'grandfather'},
+    { title: 'Grand Mother', img:GrandMother, alt:'Grandmother'},
+   ];
+
+   
+
+   return (
+    <div>
+    <Header/>
+    <Sidebar/>
+    <main>
+      <section className='video-grid'>
+        {contentItems.map((item,index) =>(
+          <div key={index} className='video-preview'>
+              <div className='thumbnail-row'>
+                <img className='thumbnail' src={item.img} alt={item.alt}/>
+              </div>
+              <div className='video-info'>
+                <p className='video-title'>{item.title}</p>
+              </div>
+          </div>
+        ))}
+      </section>
+    </main>
+    </div>
+   )
 }
 
 export default Contacts
--- src/Others.js
+++ src/Others.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import HouseKeeper from './imageFolder/HouseKeeper.png'
+import Uncle from './imageFolder/Uncle.png'
+import Sheik from './imageFolder/Sheik.png'
 
 const Others = () => {
-  return (
-    <div>Others</div>
-  )
+  const contentItems =[
+    { title: 'House Keeper', img:HouseKeeper, alt:'housekeeper'},
+    { title: 'Uncle', img:Uncle, alt:'uncle'},
+    { title: 'Sheik', img:Sheik, alt:'sheik'},
+   ];
+
+   
+
+   return (
+    <div>
+    <Header/>
+    <Sidebar/>
+    <main>
+      <section className='video-grid'>
+        {contentItems.map((item,index) =>(
+          <div key={index} className='video-preview'>
+              <div className='thumbnail-row'>
+                <img className='thumbnail' src={item.img} alt={item.alt}/>
+              </div>
+              <div className='video-info'>
+                <p className='video-title'>{item.title}</p>
+              </div>
+          </div>
+        ))}
+      </section>
+    </main>
+    </div>
+   )
 }
 
 export default Others
